471f7ae928dd64135364e01a7b52fd6a351f02ac
liusijia <lusial@wicresoft.com>
2021-06-11 17:12:50 +0800

    modify

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+// 引入这个为了记录页面上的性能
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
+  // 在app的外侧包裹React.StrictMode 可以检查代码规范 比如说即将弃用的一些api
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,4 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+// 应用的整体测试 应用的单元测试 组件测试 jest-dom 第三方库
