1bfc4df753eb321941ccac3a0b3f6234e10aedc6
liusijia <lusial@wicresoft.com>
2021-06-21 14:46:09 +0800

    change path

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
 import React from 'react'
+import { Button } from 'antd'
+import './App.css'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">Hello</div>
+        <Button type="primary">放松放松</Button>
       </div>
     )
   }
